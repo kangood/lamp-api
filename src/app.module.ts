04ca2105d728991eb4912d951e0b6238bb6b7ce0
kangod <kangod.yan@gmail.com>
2023-07-29 13:42:27 +0800

    1、配置多环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 import { database } from './config';
@@ -7,14 +8,32 @@ import { DatabaseModule } from './modules/database/database.module';
 import { OrgModule } from './modules/org/org.module';
 import { SystemModule } from './modules/system/system.module';
 
+const envFilePath = ['.env'];
+
+export const IS_DEV = process.env.RUNNING_ENV !== 'prod';
+
+if (IS_DEV) {
+    envFilePath.unshift('.env.dev');
+} else {
+    envFilePath.unshift('.env.prod');
+}
+
 @Module({
-    imports: [DatabaseModule.forRoot(database), SystemModule, OrgModule],
+    imports: [
+        DatabaseModule.forRoot(database),
+        SystemModule,
+        OrgModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath,
+        }),
+    ],
     providers: [
         {
             provide: APP_PIPE,
             useValue: new AppPipe({
                 transform: true,
-                forbidUnknownValues: true,
+                forbidUnknownValues: false,
                 validationError: { target: false },
             }),
         },
@@ -26,6 +45,7 @@ import { SystemModule } from './modules/system/system.module';
             provide: APP_FILTER,
             useClass: AppFilter,
         },
+        Logger,
     ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { useContainer } from 'class-validator';
 
 import { AppModule } from './app.module';
+
+const { PROTOCOL, HOST, PORT } = process.env;
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
@@ -12,6 +15,8 @@ async function bootstrap() {
     useContainer(app.select(AppModule), {
         fallbackOnErrors: true,
     });
-    await app.listen(3000);
+    await app.listen(PORT, () => {
+        Logger.log(`服务已经启动,接口请访问:${PROTOCOL}${HOST}:${PORT}`);
+    });
 }
 bootstrap();
